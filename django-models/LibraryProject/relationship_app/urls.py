5297ccac358fcdc445bb3bf9e021d7ce8ad35582
shema Raymond <65981217+shema-hub@users.noreply.github.com>
2025-02-23 15:35:42 +0200

    Refactor URL imports to use built-in authentication views for user login and logout

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, register, UserLoginView, UserLogoutView
+from .views import list_books, LibraryDetailView, register
+from django.contrib.auth.views import LoginView as UserLoginView, LogoutView as UserLogoutView
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),  # URL for listing all books
