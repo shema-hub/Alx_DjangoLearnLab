be6869481243fdc6fb33d9367928730d802842ee
shema Raymond <65981217+shema-hub@users.noreply.github.com>
2025-02-23 14:42:53 +0200

    Fix LibraryDetailView template path and clean up imports in views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Book
+from django.views.generic import DetailView
+from .models import Library
 
 
 
@@ -14,11 +16,10 @@ def list_books(request):
 
 
 
-from django.views.generic import DetailView
-from .models import Library
+
 
 # Class-based view to display library details
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
