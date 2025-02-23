708fbbabf553847a3dee7c26f1dd64ebae80413b
shema Raymond <65981217+shema-hub@users.noreply.github.com>
2025-02-23 14:49:19 +0200

    Refactor URL patterns to import specific views for better clarity

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from . import views
+from . import views 
+from views import list_books, LibraryDetailView
 
 urlpatterns = [
     # URL for function-based view to list books
