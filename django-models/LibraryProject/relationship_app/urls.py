1868ae81a91e6f50ea0335f40f05c9286d0ec6c7
shema Raymond <65981217+shema-hub@users.noreply.github.com>
2025-02-23 15:27:55 +0200

    Replace built-in authentication views with custom user login and logout views in URL patterns

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView, register, UserLoginView, UserLogoutView
-from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),  # URL for listing all books
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # URL for library details
     path('register/', register, name='register'),  # URL for user registration
-    path('login/', LoginView.as_view(template_name='login.html'), name='login'),  # URL for login
-    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),  # URL for logout
+    path('login/', UserLoginView.as_view(), name='login'),  # URL for login
+    path('logout/', UserLogoutView.as_view(), name='logout'),  # URL for logout
 ]
