5d29f79f7d0021ce8dbd65288d72d936e79ef6fc
shema Raymond <65981217+shema-hub@users.noreply.github.com>
2025-02-24 20:37:23 +0200

    Add comment to register URL pattern for clarity

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,12 +2,11 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import home, list_books, register, admin_view, librarian_view, member_view, LibraryDetailView
 
-
 urlpatterns = [
     path("", home, name="home"),
     path("books/", list_books, name="list_books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-    path("register/", register, name="register"),
+    path("register/", register, name="register"),  # Iyi ni yo nzira ikenewe
     path("login/", LoginView.as_view(template_name="registration/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="registration/logout.html"), name="logout"),
     path("admin_view/", admin_view, name="admin_view"),
