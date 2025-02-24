be1e754ee5a65b6f880b0c891475975f743400fa
shema Raymond <65981217+shema-hub@users.noreply.github.com>
2025-02-24 20:38:38 +0200

    Remove comment from register URL pattern

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", home, name="home"),
     path("books/", list_books, name="list_books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-    path("register/", register, name="register"),  # Iyi ni yo nzira ikenewe
+    path("register/", register, name="register"),
     path("login/", LoginView.as_view(template_name="registration/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="registration/logout.html"), name="logout"),
     path("admin_view/", admin_view, name="admin_view"),
